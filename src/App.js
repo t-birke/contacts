e6b8656eb8e15b9d05d0e636da71219264831832
Thomas Birke <tbirke@salesforce.com>
2018-04-26 22:52:46 +0200

    finish course

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
-import './index.css';
-
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
-    contacts: [
-     {
-       "id": "karen",
-       "name": "Karen Isgrigg",
-       "handle": "@karen_isgrigg",
-       "avatarURL": "http://localhost:5001/karen.jpg"
-     },
-     {
-       "id": "richard",
-       "name": "Richard Kalehoff",
-       "handle": "@richardkalehoff",
-       "avatarURL": "http://localhost:5001/richard.jpg"
-     },
-     {
-       "id": "tyler",
-       "name": "Tyler McGinnis",
-       "handle": "@tylermcginnis",
-       "avatarURL": "http://localhost:5001/tyler.jpg"
-     }
-    ]
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
   }
   removeContact = (contact) => {
     this.setState((currentState) => ({
@@ -32,16 +22,35 @@ class App extends Component {
         return c.id !== contact.id
       })
     }))
+    ContactsAPI.remove(contact)
+  }
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
   }
   render() {
     return (
       <div>
-        <ListContacts 
-          contacts={this.state.contacts} 
-          onDeleteContact={this.removeContact}
+        <Route exact path='/' render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
           />
+          )} />
+
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}/>
+        )} />
       </div>
-    );
+    )
   }
 }
 
